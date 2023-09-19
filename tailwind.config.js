8ef40278aec8d264a344a2ec141fada998c14283
Yaroslav Golubev <my3blkant50@gmail.com>
2023-09-19 15:12:41 +0500

    add Poppins font as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         color: {
           warning: '#FF8A00',
